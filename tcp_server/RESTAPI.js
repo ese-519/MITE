42f9c023db3d628985792447892f94cb615137a0
rspice <rspicer@gmail.com>
2016-11-12 19:54:13 -0500

    TCP server improvements
    - Removed fixed timeout for function return from Photon
    - Moved event listeners inside POST functions to return data in real
    time
    - Added provisions using bodyParser to handle incoming JSON data from
    video player

--- tcp_server/RESTAPI.js
+++ tcp_server/RESTAPI.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+//var bodyParser = require("body-parser");
 
 var os = require('os');
 var net = require('net');
@@ -31,17 +32,15 @@ var incoming = "";
 var server1 = net.createServer(function(socket){ //stupid anon functions.
   console.log("Someone connected from " + socket.remoteAddress + ":" + socket.remotePort + " to my TCP client!");
   conns.push(socket);
-  socket.on("data", function (data){
-    incoming = data[0];
-    data = "";
-  });
 });
 server1.listen(PORT);
-
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.post('/ledon', function (req, res) {
       //console.log(req);
       sock = conns[0];
       sock.write('a');
+	  //console.log(req.body.args);		// Provisions to actually read JSON data from the requester
       res.writeHead(200, {"Content-Type": "text/plain"});
       res.end( "Turning LED ON (*)\n" );
 });
@@ -54,28 +53,34 @@ app.post('/ledoff', function (req, res) {
       res.end( "Turning LED OFF ( )\n" );
 });
 
-app.post('/button', function (req, res) {
-      //console.log(req);
-      sock = conns[0];
-      incoming = "";
-      sock.write('c');
-      res.writeHead(200, {"Content-Type": "text/plain; charset = utf-8"});
-      setTimeout(function() {
-        var ans = incoming;
-        console.log(ans);
-        res.end(ans + "\n");
-            },2500);
+app.post('/button', function(req, res) {
+	var sock = conns[0];
+	sock.write('c');
+	res.writeHead(200, {"Content-Type": "text/plain; charset = utf-8"});
+	var dataFlag = false;
+	sock.once('data', function(data) {
+		dataFlag = true;
+		if (dataFlag) {
+			console.log(data[0] + " from button");
+			res.end(data[0] + "\n");
+		}
+	});
 });
-/*
-app.get('/button', function(req, res){
-    sock = conns[0];
-    res.writeHead(200, {"Content-Type": "text/html"});
-    var ans = incoming;
-    incoming = "";
-    console.log(ans);
-    res.end(ans + "\n");
+
+app.post('/button2', function(req, res) {
+	var sock = conns[0];
+	sock.write('c');
+	res.writeHead(200, {"Content-Type": "text/plain; charset = utf-8"});
+	var dataFlag = false;
+	sock.once('data', function(data) {
+		dataFlag = true;
+		if (dataFlag) {
+			console.log(data[0] + " from button2");
+			res.end(data[0] + "\n");
+		}
+	});
 });
-*/
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
