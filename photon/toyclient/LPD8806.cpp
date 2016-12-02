8cf759b61a0ed24ce0617eb145adb41fa99a79c0
Nichin Sreekantaswamy <nichins@seas.upenn.edu>
2016-12-02 01:31:23 -0500

    added everything

---
+++ photon/toyclient/LPD8806.cpp
@@ -0,0 +1,228 @@
+/*
+Arduino library to control LPD8806-based RGB LED Strips
+Copyright (C) Adafruit Industries
+MIT license
+
+Clearing up some misconceptions about how the LPD8806 drivers work:
+
+The LPD8806 is not a FIFO shift register.  The first data out controls the
+LED *closest* to the processor (unlike a typical shift register, where the
+first data out winds up at the *furthest* LED).  Each LED driver 'fills up'
+with data and then passes through all subsequent bytes until a latch
+condition takes place.  This is actually pretty common among LED drivers.
+
+All color data bytes have the high bit (128) set, with the remaining
+seven bits containing a brightness value (0-127).  A byte with the high
+bit clear has special meaning (explained later).
+
+The rest gets bizarre...
+
+The LPD8806 does not perform an in-unison latch (which would display the
+newly-transmitted data all at once).  Rather, each individual byte (even
+the separate G, R, B components of each LED) is latched AS IT ARRIVES...
+or more accurately, as the first bit of the subsequent byte arrives and
+is passed through.  So the strip actually refreshes at the speed the data
+is issued, not instantaneously (this can be observed by greatly reducing
+the data rate).  This has implications for POV displays and light painting
+applications.  The 'subsequent' rule also means that at least one extra
+byte must follow the last pixel, in order for the final blue LED to latch.
+
+To reset the pass-through behavior and begin sending new data to the start
+of the strip, a number of zero bytes must be issued (remember, all color
+data bytes have the high bit set, thus are in the range 128 to 255, so the
+zero is 'special').  This should be done before each full payload of color
+values to the strip.  Curiously, zero bytes can only travel one meter (32
+LEDs) down the line before needing backup; the next meter requires an
+extra zero byte, and so forth.  Longer strips will require progressively
+more zeros.  *(see note below)
+
+In the interest of efficiency, it's possible to combine the former EOD
+extra latch byte and the latter zero reset...the same data can do double
+duty, latching the last blue LED while also resetting the strip for the
+next payload.
+
+So: reset byte(s) of suitable length are issued once at startup to 'prime'
+the strip to a known ready state.  After each subsequent LED color payload,
+these reset byte(s) are then issued at the END of each payload, both to
+latch the last LED and to prep the strip for the start of the next payload
+(even if that data does not arrive immediately).  This avoids a tiny bit
+of latency as the new color payload can begin issuing immediately on some
+signal, such as a timer or GPIO trigger.
+
+Technically these zero byte(s) are not a latch, as the color data (save
+for the last byte) is already latched.  It's a start-of-data marker, or
+an indicator to clear the thing-that's-not-a-shift-register.  But for
+conversational consistency with other LED drivers, we'll refer to it as
+a 'latch' anyway.
+
+* This has been validated independently with multiple customers'
+  hardware.  Please do not report as a bug or issue pull requests for
+  this.  Fewer zeros sometimes gives the *illusion* of working, the first
+  payload will correctly load and latch, but subsequent frames will drop
+  data at the end.  The data shortfall won't always be visually apparent
+  depending on the color data loaded on the prior and subsequent frames.
+  Tested.  Confirmed.  Fact.
+*/
+
+// This file was modified by Jeff Glancy (https://github.com/jeffglancy)
+//  in order to work with Particle Photon & RedBear Duo
+
+#include "application.h"
+#include "LPD8806.h"
+
+/*****************************************************************************/
+
+// Constructor for use with hardware SPI 0:
+lpd8806::lpd8806(uint16_t n) {
+  pixels = NULL;
+  begun  = false;
+  updateLength(n);
+  updatePins(0);
+}
+
+// Constructor for use with SPI port 0 or 1 defined:
+lpd8806::lpd8806(uint16_t n, uint8_t spi_num) {
+  pixels = NULL;
+  begun  = false;
+  updateLength(n);
+  updatePins(spi_num);
+}
+
+// via Michael Vogt/neophob: empty constructor is used when strip length
+// isn't known at compile-time; situations where program config might be
+// read from internal flash memory or an SD card, or arrive via serial
+// command.  If using this constructor, MUST follow up with updateLength()
+// and updatePins() to establish the strip length and output pins!
+lpd8806::lpd8806(void) {
+  numLEDs = numBytes = 0;
+  pixels  = NULL;
+  begun   = false;
+  updatePins(0); // Must assume hardware SPI until pins are set
+}
+
+// Activate hardware SPI as appropriate:
+void lpd8806::begin(void) {
+  startSPI();
+  begun = true;
+}
+
+// Change pin assignments post-constructor, using SPI port 0 or 1:
+void lpd8806::updatePins(uint8_t spi_num) {
+  if(begun) { // If begin() was previously invoked...
+    // If previously using hardware SPI, turn that off:
+    if(spi_port){
+      //SPI1.end();
+    }
+    else{
+      SPI.end();
+    }
+  }
+  spi_port = spi_num;
+}
+
+// Enable SPI hardware and set up protocol details:
+void lpd8806::startSPI(void) {
+  if (spi_port) {
+    //SPI1.begin();
+    //SPI1.setBitOrder(MSBFIRST);
+    //SPI1.setDataMode(SPI_MODE0);
+    //SPI1.setClockSpeed(2, MHZ);
+  } else {
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockSpeed(2, MHZ);
+  }
+
+
+  // SPI bus is run at 2MHz.  Although the LPD8806 should, in theory,
+  // work up to 20MHz, the unshielded wiring from the Arduino is more
+  // susceptible to interference.  Experiment and see what you get.
+
+  // Issue initial latch/reset to strip:
+  for(uint16_t i=((numLEDs+31)/32); i>0; i--) {
+    if (spi_port){
+      //SPI1.transfer(0);
+    }
+    else{
+      SPI.transfer(0);
+    }
+  }
+}
+
+// Change strip length (see notes with empty constructor, above):
+void lpd8806::updateLength(uint16_t n) {
+  uint8_t  latchBytes;
+  uint16_t dataBytes, totalBytes;
+
+  numLEDs = numBytes = 0;
+  if(pixels) free(pixels); // Free existing data (if any)
+
+  dataBytes  = n * 3;
+  latchBytes = (n + 31) / 32;
+  totalBytes = dataBytes + latchBytes;
+  if((pixels = (uint8_t *)malloc(totalBytes))) { // Alloc new data
+    numLEDs  = n;
+    numBytes = totalBytes;
+    memset( pixels           , 0x80, dataBytes);  // Init to RGB 'off' state
+    memset(&pixels[dataBytes], 0   , latchBytes); // Clear latch bytes
+  }
+  // 'begun' state does not change -- pins retain prior modes
+}
+
+uint16_t lpd8806::numPixels(void) {
+  return numLEDs;
+}
+
+void lpd8806::show(void) {
+  uint8_t  *ptr = pixels;
+  uint16_t i    = numBytes;
+
+  // This doesn't need to distinguish among individual pixel color
+  // bytes vs. latch data, etc.  Everything is laid out in one big
+  // flat buffer and issued the same regardless of purpose.
+  if (spi_port) {
+      //while(i--) SPI1.transfer(*ptr++);
+  } else {
+      while(i--) SPI.transfer(*ptr++);
+  }
+}
+
+// Convert separate R,G,B into combined 32-bit GRB color:
+uint32_t lpd8806::Color(byte r, byte g, byte b) {
+  return ((uint32_t)(g | 0x80) << 16) |
+         ((uint32_t)(r | 0x80) <<  8) |
+                     b | 0x80 ;
+}
+
+// Set pixel color from separate 7-bit R, G, B components:
+void lpd8806::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
+  if(n < numLEDs) { // Arrays are 0-indexed, thus NOT '<='
+    uint8_t *p = &pixels[n * 3];
+    *p++ = g | 0x80; // Strip color order is GRB,
+    *p++ = r | 0x80; // not the more common RGB,
+    *p++ = b | 0x80; // so the order here is intentional; don't "fix"
+  }
+}
+
+// Set pixel color from 'packed' 32-bit GRB (not RGB) value:
+void lpd8806::setPixelColor(uint16_t n, uint32_t c) {
+  if(n < numLEDs) { // Arrays are 0-indexed, thus NOT '<='
+    uint8_t *p = &pixels[n * 3];
+    *p++ = (c >> 16) | 0x80;
+    *p++ = (c >>  8) | 0x80;
+    *p++ =  c        | 0x80;
+  }
+}
+
+// Query color from previously-set pixel (returns packed 32-bit GRB value)
+uint32_t lpd8806::getPixelColor(uint16_t n) {
+  if(n < numLEDs) {
+    uint16_t ofs = n * 3;
+    return ((uint32_t)(pixels[ofs    ] & 0x7f) << 16) |
+           ((uint32_t)(pixels[ofs + 1] & 0x7f) <<  8) |
+            (uint32_t)(pixels[ofs + 2] & 0x7f);
+  }
+
+  return 0; // Pixel # is out of bounds
+}
